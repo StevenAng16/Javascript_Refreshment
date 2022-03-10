569dc9bd636dfec2e1dea8a1ce8dda3d5e1d19db
Steven <angsteven7@gmail.com>
2022-03-10 13:23:32 +0700

    Array

---
+++ ArraySpread.js
@@ -0,0 +1,20 @@
+
+//Array
+/*
+let a = [1, 2, 3]
+let b = [...a]
+console.log(b); //[1,2,3]
+console.log(a === b); //false
+*/
+/*
+const a = [1, 2, 3];
+const b = [1, 2, 3];
+const c = [...a, ...b];
+console.log(c); //[1,2,3,1,2,3]
+*/
+/*
+const a = [1, 2, 3];
+const b = [1];
+const c = [...a, ...b];
+console.log(c); //[1,2,3,1]
+*/
--- Spread.md
+++ Spread.md
@@ -1,3 +1,5 @@
 Spread 
 
-    Spread syntax dapat digunakan ketika semua elemen dari object atau array perlu di masukkan ke dalam semacam daftar
+    Spread syntax dapat digunakan ketika semua elemen dari object atau (array) perlu di masukkan ke dalam semacam daftar
+
+    (Array)
