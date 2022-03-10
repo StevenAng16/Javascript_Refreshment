74132bbdb83d4dd2bbba56f464a7aa9004f8f17a
Steven <angsteven7@gmail.com>
2022-03-10 16:28:54 +0700

    TryCatch

---
+++ TryCatch.js
@@ -0,0 +1,12 @@
+
+//Try...Catch
+
+/*
+try{
+    TidakKetemu();
+} catch (error) {
+    console.error(error);
+}
+*/
+
+//Try bertugas mengeksekusi statement di dalam nya, sedangkan catch bertugas menangkap error yang terjadi pada statement di dalam try
