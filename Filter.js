af4c08468c141127a427bd900341043387a824ed
Steven <angsteven7@gmail.com>
2022-03-10 15:29:47 +0700

    Filter

---
+++ Filter.js
@@ -0,0 +1,8 @@
+
+//Filter
+
+const angka = [1, 2, 3, 4, 5, 6];
+const result = angka.filter(nomor => nomor > 2);
+console.log(result); // [3, 4, 5, 6]
+
+//Membuat array baru dengan setaip elemen dalam array yang lulus seleksi
