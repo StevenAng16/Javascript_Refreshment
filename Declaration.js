4e22054dddeabd016f28847bbe0a8ae4f58db056
Steven <angsteven7@gmail.com>
2022-03-10 11:05:36 +0700

    Declaration

---
+++ Declaration.js
@@ -0,0 +1,67 @@
+
+//Declaration
+
+/*
+//Var
+var num1
+console.log(num1)
+//underfined
+
+//Let
+let num2
+console.log(num2)
+//underfined
+
+//Const
+//const num3
+//console.log(num3)
+//Uncaught SyntaxError: Missing intializer in const delclaration
+*/
+
+/*
+//Redeclaration
+//Var
+var num1
+var num1
+console.log(num1)
+//underfined
+
+//Let
+//let num2
+//let num2
+//console.log(num2)
+//Uncaught SyntaxError: Identifier 'num2' has already been declared
+
+//Const
+//const num3 = 1
+//const num3 = 1
+//console.log(num3)
+//Uncaught SyntaxError: Identifier 'num3' has already been declared
+*/
+
+/*
+//Reassignment
+//Var
+var num1 = 20
+console.log(num1)
+num1 = 30
+console.log(num1)
+//20
+//30
+
+//Let
+let num2 = 20
+console.log(num2)
+num2 = 30
+console.log(num2)
+//20
+//30
+
+//Const
+const num3 = 20
+console.log(num3)
+//const num3 = 30
+//console.log(num3)
+//20
+//Uncaught SyntaxError: Identifier 'num3' has already been declared
+*/
