44722f715c9c63fdb026079ab2a368ceb211ecce
Steven <angsteven7@gmail.com>
2022-03-10 15:24:59 +0700

    Slice

---
+++ Slice.js
@@ -0,0 +1,7 @@
+
+//Slice
+
+const Hewan = ['Semut', 'Kambing', 'Unta', 'Bebek', 'Gajah'];
+console.log(Hewan.slice(2)) //["Unta", "Bebek", "Gajah"]
+
+//Memilih bagian dari array, dan mengembalikan array baru
