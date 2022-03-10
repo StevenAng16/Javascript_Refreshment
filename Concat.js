54d4c227222647d7e73fd2f39f8524446f752961
Steven <angsteven7@gmail.com>
2022-03-10 15:05:15 +0700

    Concat

---
+++ Concat.js
@@ -0,0 +1,9 @@
+
+//Concat
+
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+console.log(array3); // ["a", "b", "c", "d", "e", "f"]
+
+//Menggabungkan dua atau lebih array, dan mengembalikan salinan array yang digabungkan
