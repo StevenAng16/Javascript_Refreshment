7f87306bd2dba74b88ea465a3bdd42e2e66aa38a
Steven <angsteven7@gmail.com>
2022-03-10 15:10:54 +0700

    Map

--- Map.js
+++ Map.js
@@ -4,3 +4,5 @@
 const array1 = [1, 4, 9, 16];
 const map1 = array1.map(x => x * 2);
 console.log(map1); // [2, 4, 18, 32]
+
+//Membuat array baru dengan hasi memanggil fungsi untuk setiap elemen array
